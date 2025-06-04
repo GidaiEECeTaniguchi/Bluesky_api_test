fc775db7ec1eaceec8c06da8fa3c6d2656b2bd6d
GidaiEECeTaniguchi <gidai@example.com>
2025-06-04 11:31:38 +0900

    20250604-s

--- app/src/main/java/com/testapp/bluesky_api_test/MainActivity.java
+++ app/src/main/java/com/testapp/bluesky_api_test/MainActivity.java
@@ -8,9 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import work.socialhub.kbsky.BlueskyFactory;
-import work.socialhub.kbsky.model.*;
+import work.socialhub.kbsky.api.entity.com.atproto.server.ServerCreateSessionRequest;
+import work.socialhub.kbsky.api.entity.com.atproto.server.ServerCreateSessionResponse;
+
+import work.socialhub.kbsky.api.entity.share.Response;
+
 public class MainActivity extends AppCompatActivity {
 
+    ServerCreateSessionRequest req = new ServerCreateSessionRequest();
 
+    // セッション作成（同期呼び出し）
+    Response<ServerCreateSessionResponse> res = BlueskyFactory
+            .instance(BSKY_SOCIAL_URI)
+            .server()
+            .createSession(req);
+    String accessJwt = res.get().getAccessJwt();
 }
