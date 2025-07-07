03c11e26887e93716d8769f1ada95cb98c9cf25a
GidaiEECeTaniguchi <gidai@example.com>
2025-07-07 16:02:04 +0900

    20250707-UI-s

--- app/src/main/java/com/testapp/bluesky_api_test/ui/PostActivity.java
+++ app/src/main/java/com/testapp/bluesky_api_test/ui/PostActivity.java
@@ -4,4 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.testapp.bluesky_api_test.R;
 public class PostActivity extends  AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_post);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 戻るボタンを有効にする
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
---
+++ app/src/main/java/com/testapp/bluesky_api_test/ui/PostAdapter.java
@@ -0,0 +1,53 @@
+package com.testapp.bluesky_api_test.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.testapp.bluesky_api_test.R;
+import com.testapp.bluesky_api_test.DataBaseManupilate.entity.BasePost;
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+
+    private List<BasePost> postList;
+
+    public PostAdapter(List<BasePost> postList) {
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        return new PostViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        BasePost post = postList.get(position);
+        holder.contentTextView.setText(post.getContent());
+        holder.authorTextView.setText("Author ID: " + post.getAuthor_id()); // 仮の表示
+        holder.timestampTextView.setText(post.getCreated_at()); // 仮の表示
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    static class PostViewHolder extends RecyclerView.ViewHolder {
+        TextView contentTextView;
+        TextView authorTextView;
+        TextView timestampTextView;
+
+        PostViewHolder(View itemView) {
+            super(itemView);
+            contentTextView = itemView.findViewById(R.id.text_post_content);
+            authorTextView = itemView.findViewById(R.id.text_post_author);
+            timestampTextView = itemView.findViewById(R.id.text_post_timestamp);
+        }
+    }
+}
